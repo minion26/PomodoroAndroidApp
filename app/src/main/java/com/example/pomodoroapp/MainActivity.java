60c07ec8c6b8ee5418c4e166d67e054e1a05cd70
Jitca Diana <jitcadiana6@gmail.com>
2025-01-30 15:16:56 +0200

    - pop up for break time - timer for break time - can cancel break time - the timer for studying still works in the background

--- app/src/main/java/com/example/pomodoroapp/MainActivity.java
+++ app/src/main/java/com/example/pomodoroapp/MainActivity.java
@@ -27,11 +27,15 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton btnIncrease, btnDecrease;
     private int minutes = 0; // Start time in minutes : 30
     private CountDownTimer countDownTimer;
+
+    private CountDownTimer countPauseDownTimer;
     private long timeInMillis = minutes * 60 * 1000;
 
     private Button startButton;
 
     private Boolean isRunning = false;
+
+    private long timeInMillisForPause = (long) (0.5 * 60 * 1000); // 5 minutes pause time, now is 30 seconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
         btnDecrease = findViewById(R.id.btnDecrease);
         startButton = findViewById(R.id.startButton);
 
-        updateTimerText();
+        updateTimerText(timerText, timeInMillis);
 
         btnIncrease.setOnClickListener(v -> changeTime(1)); // 5
         btnDecrease.setOnClickListener(v -> changeTime(-1)); // -5
@@ -120,7 +124,7 @@ public class MainActivity extends AppCompatActivity {
 
         minutes = 0;
         timeInMillis = minutes * 60 * 1000;
-        updateTimerText();
+        updateTimerText(timerText, timeInMillis);
 
     }
 
@@ -136,16 +140,63 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onTick(long millisUntilFinished) {
                     timeInMillis = millisUntilFinished;
+                    long elapsedMillis = ((long) minutes * 60 * 1000) - millisUntilFinished;
+
+                    //every 25 minutes, make a pop up for the pause
+                    if(elapsedMillis % ( 0.5 * 60 * 1000 ) < 1000){
+                        //make the pop up for the pause appear
+                        LayoutInflater inflaterPause = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+                        View popupViewPause = inflaterPause.inflate(R.layout.pause_pop_up, null);
+
+                        Button cancelPauseButton = popupViewPause.findViewById(R.id.cancelPauseButton);
+                        TextView timerPauseText = popupViewPause.findViewById(R.id.timerPauseText);
+
+                        // create the popup window
+                        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+                        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+                        boolean focusable = true; // lets taps outside the popup also dismiss it
+                        final PopupWindow popupWindow = new PopupWindow(popupViewPause, width, height, focusable);
+
+                        // show the popup window
+                        // which view you pass in doesn't matter, it is only used for the window tolken
+                        popupWindow.showAtLocation( startButton , Gravity.CENTER, 0, 0);
+
+                        cancelPauseButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                popupWindow.dismiss();
+                            }
+                        });
+
+                        countPauseDownTimer = new CountDownTimer(timeInMillisForPause, 1000) {
+                            @Override
+                            public void onTick(long millisUntilFinished) {
+                                timeInMillisForPause = millisUntilFinished;
+
+                                updateTimerText(timerPauseText, timeInMillisForPause);
+                            }
+
+                            @Override
+                            public void onFinish() {
+                                timerPauseText.setText("00:00");
+                                popupWindow.dismiss();
+                            }
+                        }.start();
+
+
+                    }
+
 
                     if (timeInMillis < 120000) {
                         titleText.setText(R.string.title_almost_done);
                     }
-                    updateTimerText();
+                    updateTimerText(timerText, timeInMillis);
                 }
 
                 @Override
                 public void onFinish() {
                     timerText.setText("00:00");
+                    reset();
                 }
             }.start();
 
@@ -165,14 +216,14 @@ public class MainActivity extends AppCompatActivity {
         if (minutes < 0) minutes = 1; // 30
         if(minutes > MAX_MINUTES) minutes = 120;
         timeInMillis = minutes * 60 * 1000;
-        updateTimerText();
+        updateTimerText(timerText, timeInMillis);
     }
 
     @SuppressLint("DefaultLocale")
-    private void updateTimerText() {
+    private void updateTimerText(TextView view, long timeInMillis) {
         int mins = (int) (timeInMillis / 1000 / 60);
         int secs = (int) ((timeInMillis / 1000) % 60);
 
-        timerText.setText(String.format("%02d:%02d", mins, secs));
+        view.setText(String.format("%02d:%02d", mins, secs));
     }
 }
